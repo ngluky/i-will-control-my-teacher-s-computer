e294866f0ca47c301b17c0af13010de254524b0d
ngluky <93089950+ngluky@users.noreply.github.com>
2022-05-01 21:51:50 +0700

    Delete run.py

--- run.py
+++
@@ -1,7 +0,0 @@
-import json, pyautogui ,numpy , cv2, random, string , win10toast, logging
-from flask import Flask, send_file , request , make_response, Response
-from pynput.keyboard import Controller,Key
-from pynput.mouse import Button
-from pynput.mouse import Controller as Controller_m
-
-import PIL.ImageGrab
